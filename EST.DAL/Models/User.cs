54465a47363d663b0366cdeaeb0617b570f1a5ce
Andrii Pasternak <a.pasternak@nltu.lviv.ua>
2024-03-21 13:28:43 +0200

    added email column to user, changed registration, added constant variables file

--- EST.DAL/Models/User.cs
+++ EST.DAL/Models/User.cs
@@ -2,6 +2,7 @@
 {
     public class User : BaseEntity
     {
+        public string Email { get; set; }
         public string RoleName { get; set; }
         public List<Expense> Expenses { get; set; }
         public List<Category> Categories { get; set; }
